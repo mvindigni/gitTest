62855d742c817504a5eee3090aa1df31d80e6960
joyjumper11 <joyjumper11@gmail.com>
2020-03-27 01:43:08 -0400

    rook class wip

--- chessboard.py
+++ chessboard.py
@@ -4,8 +4,20 @@ def create_board():
     board = np.zeros((8,8))
     return board
 
-board = create_board
-is_running = True
+board = create_board()
+print(board)
 
-while is_running:
-     
+is_running = False
+
+while is_running == True:
+     print("wip")
+
+#Create classes for each piece (pawn, rook, bishop, knight, king) and then have two child classes for each piece to seperate them between white and black?
+#No need for queen class since you can simply combine rook and bishop (?), can you have a child class that inherits from two different classes?
+
+class Rook:
+    def __init__(self, currentPosition):
+        self.currentPosition = currentPosition
+    def checkMove(self):
+        pass
+        #Need to take the initial position in form [x][y] then check to see if move is in either [x][0-8] or [0-8][y], if so, return true, if not, return false -> don't worry about pieces blocking way for now??
